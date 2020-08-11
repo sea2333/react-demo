b0262e8bd218b7879e8af68b7bf3fee5d85a75a9
huangdahai <huangdahai@jd.com>
2020-08-11 15:09:08 +0800

    Updates

--- example/src/app.js
+++
@@ -1,8 +0,0 @@
-/*** examples/src/app.js ***/
-import React from 'react'
-import { render } from 'react-dom'
-import ReactDemo from '../../src'
-
-const App = () => <ReactDemo />
-render(<App />, document.getElementById('root'))
-
--- src/index.js
+++
@@ -1,6 +0,0 @@
-/*** src/index.js ***/
-import React from 'react';
-const ReactDemo = () => (
- <h1>这是我的第一个 react npm 组件</h1>
-);
-export default ReactDemo;
